e5ce52e1b6dcd9b6fa28cb4d8acbf5e6812429a6
smillanpardo <santiago.millan@nivelics.co>
2024-03-24 23:29:40 -0500

    first commit

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,10 @@
+import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
+    path: '/',
+    name: HomeSceen.name,
+    builder: (context, state) => const HomeSceen(),
+  )
+]);
---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeSceen extends StatelessWidget {
+  static const name = 'home-sceen';
+  const HomeSceen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
