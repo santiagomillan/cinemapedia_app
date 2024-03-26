ed249a938064d633670415f54a76468a8fc93de2
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-25 23:20:10 -0500

    MovieMappers

---
+++ lib/infraestructure/mappers/movie_mapper.dart
@@ -0,0 +1,24 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infraestructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB movieDB) => Movie(
+      adult: movieDB.adult,
+      backdropPath: (movieDB.backdropPath != '')
+          ? 'https://image.tmdb.org/t/p/w500/${movieDB.backdropPath}'
+          : 'https://cdn.iconscout.com/icon/free/png-256/free-no-image-1771002-1505134.png',
+      genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
+      id: movieDB.id,
+      originalLanguage: movieDB.originalLanguage,
+      originalTitle: movieDB.originalTitle,
+      overview: movieDB.overview,
+      popularity: movieDB.popularity,
+      posterPath: (movieDB.posterPath != '')
+          ? 'https://image.tmdb.org/t/p/w500/${movieDB.posterPath}'
+          : 'https://cdn.iconscout.com/icon/free/png-256/free-no-image-1771002-1505134.png',
+      releaseDate: movieDB.releaseDate,
+      title: movieDB.title,
+      video: movieDB.video,
+      voteAverage: movieDB.voteAverage,
+      voteCount: movieDB.voteCount);
+}
