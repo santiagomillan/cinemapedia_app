231fe1d51d61b244ed75b41e5bf1da833a8cf098
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-27 00:40:06 -0500

    Mostrar películas en pantalla

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,6 @@
-// import 'package:cinemapedia/config/constants/enviroment.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeSceen extends StatelessWidget {
   static const name = 'home-sceen';
@@ -7,6 +8,39 @@ class HomeSceen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Placeholder());
+    return Scaffold(body: _HomeView());
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    if (nowPlayingMovies.length == 0)
+      return const Center(child: CircularProgressIndicator());
+
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        final movie = nowPlayingMovies[index];
+        return ListTile(
+          title: Text(movie.title),
+        );
+      },
+    );
   }
 }
