f94c7d284be02091684c38b708f8109ce388393a
Santiago Millan <santiagomillanpardo@gmail.com>
2024-03-28 23:52:19 -0500

    Movie Horizontal ListView p-1

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -32,7 +32,7 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    // final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final slideShowMovies = ref.watch(moviesSlideshowProvider);
 
     // if (slideShowMovies.isEmpty)
@@ -42,6 +42,11 @@ class _HomeViewState extends ConsumerState<_HomeView> {
       children: [
         const CustomAppbar(),
         MoviesSlideshow(movies: slideShowMovies),
+        MovieHorizontalListView(
+          movies: nowPlayingMovies,
+          title: 'En cartelera',
+          subTitle: 'Lunes 20',
+        )
       ],
     );
   }
