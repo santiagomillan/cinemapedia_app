bf70d052dc263002c7ea04c99fe67ae26b0aad43
Santiago Millan <santiagomillanpardo@gmail.com>
2024-05-03 21:17:13 -0500

    Leading y Actions

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,17 +1,28 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   String get searchFieldLabel => 'Search Movies';
 
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return [const Text('Build Actions')];
+    return [
+      // if (query.isNotEmpty)
+      FadeIn(
+        animate: query.isNotEmpty,
+        child: IconButton.filled(
+            onPressed: () => query = '', icon: const Icon(Icons.clear)),
+      )
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('Build Leading');
+    return IconButton(
+        onPressed: () => close(context, null),
+        icon: const Icon(Icons.arrow_back));
   }
 
   @override
